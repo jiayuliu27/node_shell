991704ac3106e56a856bfb7841df77d35cb07ceb
jiayuliu27 <jiayuliu@umich.edu>
2016-09-20 08:43:57 -0400

    update

--- bash.js
+++ bash.js
@@ -1,10 +1,10 @@
 var commands = require("./command.js");
-
+console.log(Object.keys(process));
 /**
 Required: output must be of type string
-
 **/
 function done(output) {
+	// console.log("done", output, typeof output)
 	process.stdout.write(output);
 	process.stdout.write("\nprompt > ");
 }
@@ -20,12 +20,39 @@ process.stdin.on("data", function(data) {
 	}
 
 	if(cmd[0] === "date") {
-		process.stdout.write(Date().toString());
+		done(Date().toString());
 	}
 
 	if(cmd[0] === "ls") {
-		(commands.ls());
+		// console.log(commands.ls(), typeof commands.ls())
+		commands.hello(done);
 	}
 
+	if(cmd[0] === "echo") {
+		done(commands.echo(cmd.slice(1)));
+	}
 
+	if(cmd[0] === "cat") {
+		done(commands.cat(cmd[1]));
+	}
+
+	if(cmd[0] === "head") {
+		done(commands.head(cmd[1]));
+	}
+
+	if(cmd[0] === "tail") {
+		done(commands.tail(cmd[1]));
+	}
+
+	if(cmd[0] === "sort") {
+		done(commands.sort(cmd[1]));
+	}
+
+	if(cmd[0] === "wc") {
+		done(commands.wc(cmd[1]));
+	}
+
+	if(cmd[0] === "uniq") {
+		done(commands.uniq(cmd[1]));
+	}
 });
--- command.js
+++ command.js
@@ -1,22 +1,29 @@
 var fs = require("fs");
+var request = require("request");
 
 module.exports = {
 	pwd: function() {
+		// console.log(process.env.PWD, typeof process.env.PWD)
 		return process.env.PWD;
 	},
 
-	ls: function() {
+	hello: function(done) {
+		// issue with async deferring function to the end of program
+		var output = "";
 		fs.readdir('.', function(err, files) {
-			if(err) throw err;
-			var output = "";
+			if(err) {
+				console.log(chalk.bold.red(err.toString()));
+				throw err;
+			}
 			files.forEach(function(file) {
-				// console.log(typeof file);
 				output += file.toString();
 				output += '\n';
-			});
-			console.log("---", typeof output);
-			return output;
+			});	
+			done(output);
 		});
+		// console.log("here", output, typeof output);
+		// process.stdout.write(output);
+		
 		// fs.readdir('.', function(err, files) {
 		//   if (err) throw err;
 		//   files.forEach(function(file) {
@@ -25,6 +32,47 @@ module.exports = {
 		//   process.stdout.write("prompt > ");
 		// });
 	},
+	echo: function(args) {
+		return args.join(" ");
+	},
+
+	cat: function(fileName) {
+		// readFileSync() returns a string
+		return fs.readFileSync(fileName);
+		// fs.readFile(fileName, (err, data) => {
+		// 	return data.toString();
+		// });
+
+	},
+	head: function(fileName) {
+		var content = fs.readFileSync(fileName).toString().split("\n");
+		return content.slice(0, Math.min(5, content.length)).join("\n");
+	},
+	tail: function(fileName) {
+		var content = fs.readFileSync(fileName).toString().split("\n");
+		return content.slice(Math.max(content.length - 6, 0), content.length).join("\n");
+	},
+	sort: function(fileName) {
+		var content = fs.readFileSync(fileName).toString().split("\n");
+		return content.sort().join("\n");
+
+	},
+	wc: function(fileName) {
+		var content = fs.readFileSync(fileName).toString().split("\n");
+		return content.length.toString();
+
+	},
+	uniq: function(fileName) {
+		var content = fs.readFileSync(fileName).toString().split("\n");
+		var duplicates = {};
+		return content.filter(function(line) {
+			return duplicates.hasOwnProperty(line) ? false : (duplicates[line] = true);
+		}).join("\n");
+
+	},
+	curl: function(argument) {
+		// body...
+	}
 
 
 }
